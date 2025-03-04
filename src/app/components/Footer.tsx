b4886d37396e8ea741b63841daf69730e39d8bad
Abhinav Mittal <abhinavmittal93@gmail.com>
2025-03-03 22:05:59 -0500

    Removed captcha and added LinkedIn link.

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,25 +1,25 @@
-import { FaFacebookF, FaTwitter, FaLinkedinIn, FaInstagram } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
 
 export default function Footer() {
     return (
         <footer className="bg-bg border-t border-gray-700 py-8">
             <div className="max-w-7xl mx-auto px-6 text-center">
                 <div className="flex justify-center space-x-6 mb-4">
-                    <a href="#" className="text-primary hover:text-primary transition">
+                    {/*<a href="#" className="text-primary hover:text-primary transition">
                         <FaFacebookF size={20} />
                     </a>
                     <a href="#" className="text-primary hover:text-primary transition">
                         <FaTwitter size={20} />
-                    </a>
-                    <a href="#" className="text-primary hover:text-primary transition">
+                    </a>*/}
+                    <a href="https://www.linkedin.com/company/novel-techworks" className="text-primary hover:text-primary transition">
                         <FaLinkedinIn size={20} />
                     </a>
-                    <a href="#" className="text-primary hover:text-primary transition">
+                    {/*<a href="#" className="text-primary hover:text-primary transition">
                         <FaInstagram size={20} />
-                    </a>
+                    </a>*/}
                 </div>
                 <p className="text-sm text-white">
-                    &copy; 2025 Novel TechWorks. All rights reserved.
+                    &copy; 2025 Novel TechWorks Ltd. All rights reserved.
                 </p>
             </div>
         </footer>
